16e68d5bad781d4294ab653047148ca783d6a782
jakefrenzel <jakef1@hotmail.com>
2025-03-16 15:48:07 -0700

    File structure, components, layouts, etc. All started and now making more components

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/app/components/ui/Header';
+import Sidebar from '@/app/components/ui/Sidebar';
+
+export default function Page(){
+    return (
+        <>
+            <Header />
+            <Sidebar />
+        </>
+    );
+}
