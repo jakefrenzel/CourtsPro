05771262d4806103db0baceb1311c180f5e9a563
jakefrenzel <jakef1@hotmail.com>
2025-03-18 10:28:39 -0700

    Add Main component and integrate with Navigation; update global styles and developer menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import '@/app/globals.css';
-import Link from 'next/link';
 
 export default function Page() {
   return (
-    <>
+    <div className="developer-menu">
       <h1>CourtsPro Developer Menu</h1>
       <nav>
         <h4>Press this to navigate to project</h4>
-        <a href='./admin'>Admin Dashboard</a>
+        <a href='/corporate'>Corporate Dashboard</a>
+        <a href='/admin'>Admin Dashboard</a>
+        <a href='/employee'>Employee Dashboard</a>
       </nav>
-    </>
+    </div>
   );
 }
